7020bfef0c4dba795ccd94bf7e49018042dbe68e
Zahrina Anwar <zahrinaanwar@gmail.com>
2021-08-26 19:52:08 +0800

    update alur page 1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Login from './auth/login';
 import Register from './auth/register';
-import Home from './permintaan/permintaan06';
+import Home from './alur/alur01';
 import LoadingStart from './loading';
 import Account from './account';
 
