981764010c559430199cac01923d6f148b7a4977
Zahrina Anwar <zahrinaanwar@gmail.com>
2021-08-27 16:59:37 +0800

    update alur dan info stok

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Login from './auth/login';
 import Register from './auth/register';
-import Home from './alur/alur01';
+import Home from './mobilUnit/mobilUnit01';
 import LoadingStart from './loading';
 import Account from './account';
 
