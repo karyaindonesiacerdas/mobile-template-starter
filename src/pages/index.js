d0a8c711065e4c6a5aa42bddae30cd200871b412
Zahrina Anwar <zahrinaanwar@gmail.com>
2021-08-27 18:31:43 +0800

    update mobil unit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Login from './auth/login';
 import Register from './auth/register';
-import Home from './mobilUnit/mobilUnit01';
+import Home from './mobilUnit/mobilUnit02';
 import LoadingStart from './loading';
 import Account from './account';
 
