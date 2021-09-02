b9b2d968c98562bc12b3c15ec4c061b8f05d04a8
Ihsanul <ihsanboga6415@gmail.com>
2021-09-02 19:36:47 +0700

    fix: alur

--- src/route/index.js
+++ src/route/index.js
@@ -27,6 +27,12 @@ import {
     InfoStok03,
     MobilUnit01,
     MobilUnit02,
+    Alur01,
+    Alur02,
+    Alur03,
+    Alur04,
+    Alur05,
+    Alur06,
 } from '../pages';
 
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
@@ -155,6 +161,36 @@ function index() {
                 <Stack.Screen
                     name="MobilUnit02"
                     component={MobilUnit02}
+                    options={{headerShown: false}}
+                />
+                <Stack.Screen
+                    name="Alur01"
+                    component={Alur01}
+                    options={{headerShown: false}}
+                />
+                <Stack.Screen
+                    name="Alur02"
+                    component={Alur02}
+                    options={{headerShown: false}}
+                />
+                <Stack.Screen
+                    name="Alur03"
+                    component={Alur03}
+                    options={{headerShown: false}}
+                />
+                <Stack.Screen
+                    name="Alur04"
+                    component={Alur04}
+                    options={{headerShown: false}}
+                />
+                <Stack.Screen
+                    name="Alur05"
+                    component={Alur05}
+                    options={{headerShown: false}}
+                />
+                <Stack.Screen
+                    name="Alur06"
+                    component={Alur06}
                     options={{headerShown: false}}
                 />
             </Stack.Navigator>
