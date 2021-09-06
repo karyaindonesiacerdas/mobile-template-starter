65b3c7c38bc7c772a82878d8a9c0e937d17b4650
iamadityah31 <iamditya4@gmail.com>
2021-09-06 18:45:21 +0700

    kuisioner

--- src/pages/konvalesen/konvalesen04.js
+++ src/pages/konvalesen/konvalesen04.js
@@ -1,220 +1,281 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import { Alert, ImageBackground, Image, Text, View,TextInput } from "react-native";
+import { CheckBox } from 'react-native-elements';
 import {
-  Alert,
-  ImageBackground,
-  Image,
-  Text,
-  View,
-  TextInput,
-  TouchableOpacity,
-} from "react-native";
-import { CheckBox } from "react-native-elements";
-import { Container, Header, Title, Left, HStack, Card } from "native-base";
+  Container,
+  Header,
+  Title,
+  Left,
+   HStack,
+ 
+  Card,
+  
+} from "native-base";
 import {
   ScrollView,
-  TouchableWithoutFeedback,
 } from "react-native-gesture-handler";
-import styles from "./styles";
+import {TouchableOpacity} from 'react-native'
+import styles from "../styles/styles";
+import Bg from '../../image/Baground2.jpg'
+import { Button } from "react-native-elements/dist/buttons/Button";
+
 
 function Konvalesen04(props) {
-  const [text, onChangeText] = React.useState("Useless Text");
   const [number, onChangeNumber] = React.useState(null);
-  const goNextPage = (page) => {
-    if (page) {
-      props.navigation.replace(page);
-    }
-  };
+  const [pekerjaan, setPekerjaan] = React.useState([
+    { label: 'PNS', value: 'pns', checked: false },
+    { label: 'Swasta', value: 'swasta', checked: false },
+    { label: 'Polri', value: 'polri', checked: false },
+    { label: 'Petani', value: 'petani', checked: false },
+    { label: 'BUMN', value: 'bumn', checked: false },
+    { label: 'Pelajar', value: 'pelajar', checked: false },
+    { label: 'Wirausaha', value: 'wirausaha', checked: false },
+    { label: 'Lain-lain', value: 'lain-lain', checked: false },
+  ])
+  const [gologanDarah, setGolonganDarah] = React.useState([
+    { label: 'A', value: 'A', checked: false },
+    { label: 'B', value: 'B', checked: false },
+    { label: 'O', value: 'O', checked: false },
+    { label: 'AB', value: 'AB', checked: false },
+    { label: 'Tidak Tahu', value: 'X', checked: false },
+  ])
+  const [rhesus, setRhesus] = React.useState([
+    { label: 'Positif', value: '+', checked: false },
+    { label: 'Negatif', value: '-', checked: false },
+    { label: 'Tidak Tahu', value: 'X', checked: false },
+  ])
+  const [input] = useState({
+    pekerjaan : '',
+    golongan_darah: '',
+    rhesus : '',
+  })
+  const pekerjaanHandler = (index) => {
+    const newValue = pekerjaan.map((checkbox, i) => {
+     if (i !== index)
+       return {
+         ...checkbox,
+         checked: false,
+       }
+     if (i === index) {
+       const item = {
+         ...checkbox,
+         checked: !checkbox.checked,
+       }
+       input.pekerjaan = checkbox.value
+       return item
+     }
+    return checkbox
+  })
+  setPekerjaan(newValue)
+  }
+  const golonganDarahHandler = (index) => {
+    const newValue = gologanDarah.map((checkbox, i) => {
+     if (i !== index)
+       return {
+         ...checkbox,
+         checked: false,
+       }
+     if (i === index) {
+       const item = {
+         ...checkbox,
+         checked: !checkbox.checked,
+       }
+       input.golongan_darah = checkbox.value
+       return item
+     }
+    return checkbox
+  })
+  setGolonganDarah(newValue)
+  }
 
+  const rhesusHandler = (index) => {
+    const newValue = rhesus.map((checkbox, i) => {
+     if (i !== index)
+       return {
+         ...checkbox,
+         checked: false,
+       }
+     if (i === index) {
+       const item = {
+         ...checkbox,
+         checked: !checkbox.checked,
+       }
+       input.rhesus = checkbox.value
+       return item
+     }
+    return checkbox
+  })
+  setRhesus(newValue)
+  }
+
+  const goNextPage = page => {
+    if (page) {
+      props.navigation.replace(page)
+    }
+  }
+  console.log(input)
   return (
     <Container>
+      <Image source={Bg} style={{width: '100%', height: '100%', position: 'absolute'}} />
       <Image
-        source={require("../../asset/logoUDD.png")}
+        source={require("../image/logo.png")}
         style={{
           width: 54,
           height: 60,
-          top: 10,
-          margin: 20,
-
-          left: 10,
+          top:10,
+          margin:20,
+    
+          left:10,
         }}
       ></Image>
       <Image
-        source={require("../../asset/logoSehat.png")}
+        source={require("../image/Logo2.png")}
         style={{
-          position: "absolute",
+          position:'absolute',
           width: 54,
           height: 60,
-          margin: 20,
-
-          right: 10,
-          top: 10,
+          margin:20,
+       
+          right:10,
+          top:10,
         }}
       ></Image>
       <ScrollView>
-        <Text
-          style={{
-            marginLeft: 30,
-            marginTop: 25,
-            fontSize: 35,
-            fontWeight: "bold",
-            color: "red",
-          }}
-        >
-          Daftar
-        </Text>
-        <Text
-          style={{
-            marginLeft: 30,
-            marginTop: -10,
-            fontSize: 25,
-            fontWeight: "bold",
-            color: "black",
-          }}
-        >
-          Donor Darah Konvalesen
-        </Text>
+     
+  
+     
+        <Text style={{ marginLeft:30, marginTop:0,fontSize: 35,fontWeight: "bold",  color: "red" }}>
+                Daftar
+              </Text>
+              <Text style={{ marginLeft:30, marginTop:-10,fontSize: 25,fontWeight: "bold",  color: "black" }}>
+                Donor Darah Biasa
+              </Text>
+               <Text style={{ marginLeft:30, marginTop:30,fontSize: 15,fontWeight: "bold", color: "black" }}>
+                Pekerjaan (Pilih salah satu)
+              </Text>
+              <View style={{marginTop:10,marginLeft:30,marginRight:40, flexDirection: "row",justifyContent: "space-between"}}>
+              <View>
+              {pekerjaan.map((checkbox, i) => {
+                if (i < pekerjaan.length/2){
+                  return <CheckBox
+                  style={{width:"70%"}}
+                    title={checkbox.label}
+                    checked={checkbox.checked}
+                    onPress={() => pekerjaanHandler(i)}
+                    key={i}
+                  /> 
+                }
+              })
+                  }
+              </View>
+              <View>
+              {pekerjaan.map((checkbox, i) => {
+                if (i >= pekerjaan.length/2){
+                  return <CheckBox
+                  style={{width:"70%"}}
+                    title={checkbox.label}
+                    checked={checkbox.checked}
+                    onPress={() => pekerjaanHandler(i)}
+                    key={i}
+                  /> 
+                }
+              })}
+              </View>
+              </View>
 
-        <Text
-          style={{ marginLeft: 30, marginTop: 0, fontSize: 15, color: "black" }}
-        >
-          Tempat dan Tanggal Lahir (update otomatis) apabila usia kurang dari 17
-          tahun tidak lolos (otomatis)
-        </Text>
-        <Text
-          style={{
-            marginLeft: 30,
-            marginTop: 30,
-            fontSize: 15,
-            fontWeight: "bold",
-            color: "black",
-          }}
-        >
-          Pekerjaan (Pilih salah satu)
-        </Text>
-
-        <View
-          style={{
-            marginTop: 10,
-            marginLeft: 40,
-            marginRight: 40,
-            flexDirection: "row",
-            justifyContent: "space-between",
-          }}
-        >
-          <View style={{}}>
-            <CheckBox title="PNS" style={{ width: "70%" }} />
-
-            <CheckBox title="Swasta" style={{ width: "70%" }} />
-            <CheckBox title="Polri" style={{ width: "70%" }} />
-            <CheckBox title="Petani" style={{ width: "70%" }} />
-          </View>
-
-          <View>
-            <CheckBox title="BUMN" style={{ width: "70%" }} />
-            <CheckBox title="Pelajar" style={{ width: "70%" }} />
-            <CheckBox title="Wirausaha" style={{ width: "70%" }} />
-            <CheckBox title="Lain-lain" style={{ width: "70%" }} />
-          </View>
-        </View>
-
-        <Text
-          style={{
-            marginLeft: 30,
-            marginTop: 20,
-            fontSize: 15,
-            fontWeight: "bold",
-            color: "black",
-            textShadowColor: "#fff",
-            textShadowOffset: { width: 1, height: 1 },
-            textShadowRadius: 10,
-          }}
-        >
-          Golongan Darah
-        </Text>
-
-        <View
-          style={{
-            marginTop: 10,
-            marginLeft: 40,
-            marginRight: 40,
-            flexDirection: "row",
-            justifyContent: "space-between",
-          }}
-        >
-          <View style={{}}>
-            <CheckBox title="A" style={{ width: "70%" }} />
-
-            <CheckBox title="B" style={{ width: "70%" }} />
-            <CheckBox title="O" style={{ width: "70%" }} />
-          </View>
-
-          <View>
-            <CheckBox title="AB" style={{ width: "70%" }} />
-            <CheckBox title="X (Tidak tahu)" style={{ width: "70%" }} />
-          </View>
-        </View>
-
-        <Text
-          style={{
-            marginLeft: 30,
-            marginTop: 20,
-            fontSize: 15,
-            fontWeight: "bold",
-            color: "black",
-            textShadowColor: "#fff",
-            textShadowOffset: { width: 1, height: 1 },
-            textShadowRadius: 10,
-          }}
-        >
-          Rhesus
-        </Text>
-
-        <View
-          style={{
-            marginTop: 10,
-            marginLeft: 40,
-            marginRight: 40,
-            flexDirection: "row",
-            justifyContent: "space-between",
-          }}
-        >
-          <View style={{}}>
-            <CheckBox title="Positif" style={{ width: "70%" }} />
-            <CheckBox title="X (Tidak Tahu)" style={{ width: "70%" }} />
-          </View>
-
-          <View>
-            <CheckBox title="Negatif" style={{ width: "70%" }} />
-          </View>
-        </View>
-
+               <Text style={{ marginLeft:30, marginTop:20,fontSize: 15,fontWeight: "bold", color: "black", textShadowColor:'#fff',
+    textShadowOffset:{width: 1, height: 1},
+    textShadowRadius:10,
+   }}>
+                Golongan Darah
+              </Text>
+              <View style={{marginTop:10,marginLeft:30,marginRight:40, flexDirection: "row",justifyContent: "space-between"}}>
+              <View>
+              {gologanDarah.map((checkbox, i) => {
+                if (i < gologanDarah.length/2){
+                  return <CheckBox
+                  style={{width:"70%"}}
+                    title={checkbox.label}
+                    checked={checkbox.checked}
+                    onPress={() => golonganDarahHandler(i)}
+                    key={i}
+                  /> 
+                }
+              })
+                  }
+                  </View>
+                  <View>
+              {gologanDarah.map((checkbox, i) => {
+                if (i >= gologanDarah.length/2){
+                  return <CheckBox
+                  style={{width:"70%"}}
+                    title={checkbox.label}
+                    checked={checkbox.checked}
+                    onPress={() => golonganDarahHandler(i)}
+                    key={i}
+                  /> 
+                }
+              })}
+              </View>
+              </View>
+              <Text style={{ marginLeft:30, marginTop:20,fontSize: 15,fontWeight: "bold", color: "black" , textShadowColor:'#fff',
+    textShadowOffset:{width: 1, height: 1},
+    textShadowRadius:10,}}>
+                Rhesus
+              </Text>
+              <View style={{marginTop:10,marginLeft:30,marginRight:40, flexDirection: "row",justifyContent: "space-between"}}>
+              <View>
+              {rhesus.map((checkbox, i) => {
+                if (i < rhesus.length/2){
+                  return <CheckBox
+                  style={{width:"70%"}}
+                    title={checkbox.label}
+                    checked={checkbox.checked}
+                    onPress={() => rhesusHandler(i)}
+                    key={i}
+                  /> 
+                }
+              })
+                  }
+                  </View>
+                  <View>
+              {rhesus.map((checkbox, i) => {
+                if (i >= rhesus.length/2){
+                  return <CheckBox
+                  style={{width:"70%"}}
+                    title={checkbox.label}
+                    checked={checkbox.checked}
+                    onPress={() => rhesusHandler(i)}
+                    key={i}
+                  /> 
+                }
+              })}
+              </View>             
+              </View>
+              
         <View
           style={{
             alignContent: "center",
 
             flexDirection: "row",
             justifyContent: "center",
-            alignContent: "center",
-            marginTop: 30,
-            marginBottom: 10,
+              alignContent: "center",
+              marginTop:30,
+            
           }}
         >
           <Card
             style={{
-              backgroundColor: "#000",
-              width: "40%",
-              marginRight: "2%",
-            }}
+              backgroundColor: "#000",width: "40%", marginRight:"2%" }}
           >
-            <TouchableOpacity onPress={goNextPage.bind(this, "Konvalesen03")}>
+            <TouchableOpacity style={styles.button} onPress={goNextPage.bind(this, 'Konvalesen03')} >
               <Text
                 style={{
                   margin: 10,
                   fontSize: 20,
-
+             
                   color: "white",
-                  fontWeight: "bold",
-                  textAlign: "center",
+                  fontWeight: "bold",textAlign:'center',
                 }}
               >
                 Kembali
@@ -223,17 +284,14 @@ function Konvalesen04(props) {
           </Card>
           <Card
             style={{
-              backgroundColor: "#000",
-              width: "40%",
-              marginLeft: "2%",
+              backgroundColor: "#000",width: "40%",marginLeft:"2%"
             }}
           >
-            <TouchableOpacity onPress={goNextPage.bind(this, "Konvalesen05")}>
+            <TouchableOpacity style={styles.button} onPress={goNextPage.bind(this, 'Konvalesen05')} >
               <Text
                 style={{
                   margin: 10,
-                  fontSize: 20,
-                  textAlign: "center",
+                  fontSize: 20, textAlign:'center',
 
                   color: "white",
                   fontWeight: "bold",
@@ -244,26 +302,8 @@ function Konvalesen04(props) {
             </TouchableOpacity>
           </Card>
         </View>
-      </ScrollView>
-
-      <ImageBackground
-        // resizeMethod={'auto'}
-        source={require("../../asset/footer.png")}
-        style={{
-          width: "100%",
-
-          backgroundColor: "#fff",
-          padding: 0,
-          paddingVertical: 90,
-          position: "absolute",
-          zIndex: -1,
-          bottom: 0,
-        }}
-        imageStyle={{
-          resizeMode: "cover",
-          alignSelf: "flex-end",
-        }}
-      ></ImageBackground>
+          
+ </ScrollView>
     </Container>
   );
 }
