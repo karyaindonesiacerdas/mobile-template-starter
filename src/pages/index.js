f5cba5b32faf1fcc78b4b5507f29c68a8fe17eee
Hady Eka Saputra <hadyekasaputra@gmail.com>
2021-09-08 14:35:56 +0700

    changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Login from './auth/login';
 import Register from './auth/register';
-import Home from './home';
+import Home from './alur/alur02';
 import LoadingStart from './loading';
 import Account from './account';
 import KegiatanDonor from './kegiatan donor';
